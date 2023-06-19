f065f56ac718d10daa840ab95ab97b112432fa31
cynber <26402139+cynber@users.noreply.github.com>
2023-06-18 22:50:28 -0700

    remove instance setter button

--- content.js
+++ content.js
@@ -4,7 +4,6 @@ const communityName = targetElement.querySelector('.overflow-wrap-anywhere').tex
 if (communityName.includes('!') && !communityName.includes('@')) {
 
     const goButton = document.createElement('button');
-    // const setButton = document.createElement('button');
     const message = document.createElement('p');
 
     goButton.setAttribute('type', 'button');
@@ -28,28 +27,6 @@ if (communityName.includes('!') && !communityName.includes('@')) {
         window.location.href = redirectURL;
     });
 
-    // setButton.textContent = 'Change my selected instance';
-    // setButton.setAttribute('type', 'button');
-    // setButton.style.cssText = `
-    //     font-weight: 400;
-    //     text-align: center;
-    //     padding: .375rem .75rem;
-    //     color: white;
-    //     border: none;
-    //     border-radius: 5px;
-    //     margin: .5rem 0rem .5rem 0rem;
-    //     width: 100%;
-    // `;
-    // setButton.style.backgroundColor = '#501919';
-    // setButton.addEventListener('click', () => {
-    //     const inputInstance = prompt('Enter your instance URL:');
-    //     if (inputInstance) {
-    //         browser.storage.local.set({
-    //             selectedInstance: inputInstance.trim()
-    //         });
-    //     }
-    // });
-
     message.style.cssText = `
         font-size: 0.8rem;
         color: #666;
@@ -57,7 +34,6 @@ if (communityName.includes('!') && !communityName.includes('@')) {
     message.textContent = 'To change your selected instance, click on the extension icon in the top right corner of your browser.';
 
     targetElement.appendChild(goButton);
-    // targetElement.appendChild(setButton);
     targetElement.appendChild(message);
 
 }
