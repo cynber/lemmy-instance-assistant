0665f53bc7ecace80b333d20cc87b8ffac2a7522
cynber <26402139+cynber@users.noreply.github.com>
2023-07-02 04:26:58 -0700

    updated logic for buttons
    - added home post button & message
    - added conditional displaying
    - added buttons to post pages
    - added home post button logic

--- content.js
+++ content.js
@@ -5,11 +5,11 @@ browser.storage.local.get('selectedInstance').then(({ selectedInstance }) => {
     // Check if on a community page
     if (CURRENT_PATH.includes("/c/") || CURRENT_PATH.includes("/m/") || CURRENT_PATH.includes("/post/")) {
 
-        // Create button
-        let goButton = document.createElement('button');
-        goButton.setAttribute('type', 'button');
-        goButton.textContent = 'Open community in my home instance';
-        goButton.style.cssText = `
+        // Create home instance button
+        let homeInstanceButton = document.createElement('button');
+        homeInstanceButton.setAttribute('type', 'button');
+        homeInstanceButton.textContent = 'Open community in my home instance';
+        homeInstanceButton.style.cssText = `
             padding: .375rem .75rem;
             margin: 1rem 0rem .5rem 0rem;
             width: 100%;
@@ -19,47 +19,116 @@ browser.storage.local.get('selectedInstance').then(({ selectedInstance }) => {
             text-align: center;
             color: white;
         `;
-        goButton.style.backgroundColor = '#17305a';
+        homeInstanceButton.style.backgroundColor = '#17305a';
 
-        // Create message
-        let message = document.createElement('p');
-        message.style.cssText = `font-size: 0.8rem; color: #666;`;
-        message.textContent = 'To change your home instance, click on the extension icon in the top right corner of your browser.';
+        // Create home post button
+        let homePostButton = document.createElement('button');
+        homePostButton.setAttribute('type', 'button');
+        homePostButton.textContent = 'Open post in my home instance';
+        homePostButton.style.cssText = `
+            padding: .375rem .75rem;
+            margin: 1rem 0rem 0rem 0rem;
+            width: 100%;
+            border: none;
+            border-radius: 5px;
+            font-weight: 400;
+            text-align: center;
+            color: white;
+        `;
+        homePostButton.style.backgroundColor = '#27175a';
+
+        // Create instance message
+        let instanceMessage = document.createElement('p');
+        instanceMessage.style.cssText = `font-size: 0.8rem; color: #666;`;
+        instanceMessage.textContent = 'To change your home instance, click on the extension icon in the top right corner of your browser.';
+
+        // Create post message
+        let myPostMessage = document.createElement('p');
+        myPostMessage.style.cssText = `font-size: 0.8rem; color: #666;`;
+        myPostMessage.innerHTML = `Warning: <a href="https://github.com/cynber/lemmy-instance-assistant/wiki/Why-can't-I-jump-to-the-same-post-on-my-home-instance%3F">You are currently on a post page.</a>`;
 
         // Set up variables
-        let shouldAppend = true;
-        const TARGET_ELEMENT = document.querySelector('.card-body');
+        let displayInstanceButton = true;   // default true: append home instance button
+        let displayInstanceMessage = true;  // default true: append home instance message
+        let displayPostButton = false;      // default false: do not append home post button
+        let displayPostMessage = false;     // default false: do not append home post message
+        const TARGET_ELEMENT = document.querySelector('#sidebarMain .card-body');
         const URL_PATTERN = /^(http|https):\/\/(?:[\w-]+\.)?[\w.-]+\.[a-zA-Z]{2,}$/;
-        const SOURCE_INSTANCE = CURRENT_PATH.includes("@") ?
-            CURRENT_PATH.match(/\/[cm]\/[^/@]+@([^/]+)/)[1] : CURRENT_HOST;
-        const COMMUNITY_NAME = CURRENT_PATH.includes("/post/")
-            ? TARGET_ELEMENT.querySelector('a.community-link').getAttribute('href').substring(3)
-            : CURRENT_PATH.match(/\/[cm]\/([^/@]+)/)[1];
+        let communityName = '';
+        let sourceInstance = '';
+
+        // Get community name and source instance, depending on if on a post or community page
+        if (CURRENT_PATH.includes("/post/")) {
+
+            displayPostMessage = true;
+
+            // If post is from a different foreign community
+            const COMMUNITY_LINK = TARGET_ELEMENT.querySelector('a.community-link');
+            if (COMMUNITY_LINK && COMMUNITY_LINK.getAttribute('title').includes('@')) {
+                communityName = COMMUNITY_LINK.substring(3).match(/([^@]+)/)[0]
+
+                // This will grab the community of the current post
+                sourceInstance = CURRENT_HOST
+                // This will grab the community of the ORIGINAL post, currently not being used
+                // sourceInstance = COMMUNITY_LINK.getAttribute('href').substring(3).match(/@([^/]+)/)[1]
+
+            // If post is on the current instance
+            } else {
+                communityName = COMMUNITY_LINK.getAttribute('href').substring(3)
+                sourceInstance = CURRENT_HOST
+            }
+
+        // If on a community page (/c/ or /m/)
+        } else {
+            communityName = CURRENT_PATH.match(/\/[cm]\/([^/@]+)/)[1];
+            sourceInstance = CURRENT_PATH.includes("@") ?
+                CURRENT_PATH.match(/\/[cm]\/[^/@]+@([^/]+)/)[1] : CURRENT_HOST;
+        }
 
         // Check if selected instance is valid
         if (selectedInstance && URL_PATTERN.test(selectedInstance)) {
 
-            goButton.addEventListener('click', () => {
+            // If selected instance is the same as current instance, don't append button or message
+            const HOME_INSTANCE_HOST = selectedInstance ? new URL(selectedInstance).hostname : CURRENT_HOST;
+            (HOME_INSTANCE_HOST == CURRENT_HOST) ? displayInstanceButton = false : displayInstanceButton = true;
+
+            homeInstanceButton.addEventListener('click', () => {
                 browser.storage.local.get('selectedInstance').then(({ selectedInstance }) => {
-                    const redirectURL = selectedInstance + '/c/' + COMMUNITY_NAME + '@' + SOURCE_INSTANCE;
+                    const redirectURL = selectedInstance + '/c/' + communityName + '@' + sourceInstance;
                     window.location.href = redirectURL;
                 });
             });
 
-            // If selected instance is the same as current instance, don't append button or message
-            const HOME_INSTANCE_HOST = selectedInstance ? new URL(selectedInstance).hostname : CURRENT_HOST;
-            (HOME_INSTANCE_HOST == CURRENT_HOST) ? shouldAppend = false : shouldAppend = true;
+            
+            const FEDI_BUTTON = document.querySelector('a.btn.btn-sm.btn-animate.text-muted.py-0');
+            if (FEDI_BUTTON && FEDI_BUTTON.hasAttribute('href')) {
+                const HOME_POST_URL = new URL(FEDI_BUTTON.getAttribute('href'));
+                if (HOME_POST_URL.hostname == HOME_INSTANCE_HOST) {
+                    homePostButton.addEventListener('click', () => {
+                        window.location.href = HOME_POST_URL;
+                    });
+                    displayPostButton = true;
+                }
+            }
 
         } else {
-            goButton.addEventListener('click', () => {
+            homeInstanceButton.addEventListener('click', () => {
                 alert('You have not selected a valid instance. Please select an instance by clicking the extension popup.');
             });
         }
 
         // Append button and message if selected instance is not the same as current instance
-        if (shouldAppend) {
-            TARGET_ELEMENT.appendChild(goButton);
-            TARGET_ELEMENT.appendChild(message);
+        if (displayPostButton) {
+            TARGET_ELEMENT.appendChild(homePostButton);
+        }
+        if (displayInstanceButton) {
+            TARGET_ELEMENT.appendChild(homeInstanceButton);
+        }
+        if (displayInstanceMessage) {
+            TARGET_ELEMENT.appendChild(instanceMessage);
+        }
+        if (displayPostMessage) {
+            TARGET_ELEMENT.appendChild(myPostMessage);
         }
     }
 });
