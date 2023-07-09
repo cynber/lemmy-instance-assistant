b08d2fd8a77b35b4aec5f3fc9260a6b3f997de7f
cynber <26402139+cynber@users.noreply.github.com>
2023-07-09 06:21:21 -0700

    fix bad input for contextMenus

--- src/background.js
+++ src/background.js
@@ -21,24 +21,28 @@ browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 browser.contextMenus.onClicked.addListener(async (info, tab) => {
     if (info.menuItemId === "lemmy-sidebar" && info.linkUrl) {
-        async function loadSelectedInstance() {
-            const { selectedInstance } = await browser.storage.local.get('selectedInstance');
-            const { selectedType } = await browser.storage.local.get('selectedType');
 
-            communityPrefix = selectedType ? (selectedType === "lemmy" ? "/c/" : "/m/") : "/c/";
+        let sourceHost = new URL(info.linkUrl).hostname;
+        let sourcePath = new URL(info.linkUrl).pathname;
 
-            let sourceHost = new URL(info.linkUrl).hostname;
-            let sourcePath = new URL(info.linkUrl).pathname;
+        if (sourcePath.includes("/c/") || sourcePath.includes("/m/")) {
             const communityName = sourcePath.match(/\/[cm]\/([^/@]+)/)[1];
             const sourceInstance = sourcePath.includes("@") ?
                 sourcePath.match(/\/[cm]\/[^/@]+@([^/]+)/)[1] : sourceHost;
 
-            if (sourcePath.includes("/c/") || sourcePath.includes("/m/")) {
+            async function loadStorage() {
+                const { selectedInstance } = await browser.storage.local.get('selectedInstance');
+                const { selectedType } = await browser.storage.local.get('selectedType');
+                communityPrefix = selectedType ? (selectedType === "lemmy" ? "/c/" : "/m/") : "/c/";
                 const redirectURL = selectedInstance + communityPrefix + communityName + '@' + sourceInstance;
                 browser.tabs.update(tab.id, { url: redirectURL });
-            } else { alert('This is not a valid Lemmy or Kbin link. Please click on a link to a Lemmy or Kbin community.\n\n Lemmy post links can not be redirected because of how Lemmy works. See project GitHub for more information.'); }
+            }
+            loadStorage();
+        } else {
+            console.log('This is not a valid Lemmy or Kbin link. Please click on a link to a Lemmy or Kbin community.\n\n Lemmy post links can not be redirected because of how Lemmy works. See project GitHub for more information.')
+            browser.tabs.update(tab.id, { url: 'https://github.com/cynber/lemmy-instance-assistant/wiki/Sorry-that-didn\'t-work...' });
+            // TODO: Add a popup to explain this
         }
-        loadSelectedInstance();
     }
 });
 
@@ -46,5 +50,6 @@ browser.contextMenus.create({
     id: "lemmy-sidebar",
     title: "Instance Assistant",
     contexts: ["link"],
+    targetUrlPatterns: ["http://*/c/*", "https://*/c/*", "http://*/p/*", "https://*/p/*"],
 }, () => void browser.runtime.lastError,
 );
