712200fa6e1795e9218d1dfede64608449851dd9
cynber <26402139+cynber@users.noreply.github.com>
2023-07-12 03:37:00 -0700

    add check for firefox background script

--- src/background.js
+++ src/background.js
@@ -4,9 +4,11 @@
 // --------------------------------------
 
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === "complete") {
-        browser.tabs.executeScript(tabId, { file: "content-sidebar.js" })
+  if (changeInfo.status === "complete") {
+    if (/^https?:\/\/.*\/c\//.test(tab.url)) {
+      browser.tabs.executeScript(tabId, { file: "content-sidebar.js" })
     }
+  }
 });
 
 
@@ -15,35 +17,35 @@ browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 // --------------------------------------
 
 browser.contextMenus.onClicked.addListener(async (info, tab) => {
-    if (info.menuItemId === "lemmy-sidebar" && info.linkUrl) {
+  if (info.menuItemId === "lemmy-sidebar" && info.linkUrl) {
 
-        let sourceHost = new URL(info.linkUrl).hostname;
-        let sourcePath = new URL(info.linkUrl).pathname;
+    let sourceHost = new URL(info.linkUrl).hostname;
+    let sourcePath = new URL(info.linkUrl).pathname;
 
-        if (sourcePath.includes("/c/") || sourcePath.includes("/m/")) {
-            const communityName = sourcePath.match(/\/[cm]\/([^/@]+)/)[1];
-            const sourceInstance = sourcePath.includes("@") ?
-                sourcePath.match(/\/[cm]\/[^/@]+@([^/]+)/)[1] : sourceHost;
+    if (sourcePath.includes("/c/") || sourcePath.includes("/m/")) {
+      const communityName = sourcePath.match(/\/[cm]\/([^/@]+)/)[1];
+      const sourceInstance = sourcePath.includes("@") ?
+        sourcePath.match(/\/[cm]\/[^/@]+@([^/]+)/)[1] : sourceHost;
 
-            async function loadStorage() {
-                const { selectedInstance } = await browser.storage.local.get('selectedInstance');
-                const { selectedType } = await browser.storage.local.get('selectedType');
-                communityPrefix = selectedType ? (selectedType === "lemmy" ? "/c/" : "/m/") : "/c/";
-                const redirectURL = selectedInstance + communityPrefix + communityName + '@' + sourceInstance;
-                browser.tabs.update(tab.id, { url: redirectURL });
-            }
-            loadStorage();
-        } else {
-            browser.tabs.update(tab.id, { url: 'https://github.com/cynber/lemmy-instance-assistant/wiki/Sorry-that-didn\'t-work...' });
-            // TODO: Add a popup to explain this
-        }
+      async function loadStorage() {
+        const { selectedInstance } = await browser.storage.local.get('selectedInstance');
+        const { selectedType } = await browser.storage.local.get('selectedType');
+        communityPrefix = selectedType ? (selectedType === "lemmy" ? "/c/" : "/m/") : "/c/";
+        const redirectURL = selectedInstance + communityPrefix + communityName + '@' + sourceInstance;
+        browser.tabs.update(tab.id, { url: redirectURL });
+      }
+      loadStorage();
+    } else {
+      browser.tabs.update(tab.id, { url: 'https://github.com/cynber/lemmy-instance-assistant/wiki/Sorry-that-didn\'t-work...' });
+      // TODO: Add a popup to explain this
     }
+  }
 });
 
 browser.contextMenus.create({
-    id: "lemmy-sidebar",
-    title: "Instance Assistant",
-    contexts: ["link"],
-    targetUrlPatterns: ["http://*/c/*", "https://*/c/*", "http://*/p/*", "https://*/p/*"],
+  id: "lemmy-sidebar",
+  title: "Instance Assistant",
+  contexts: ["link"],
+  targetUrlPatterns: ["http://*/c/*", "https://*/c/*", "http://*/p/*", "https://*/p/*"],
 }, () => void browser.runtime.lastError,
 );
