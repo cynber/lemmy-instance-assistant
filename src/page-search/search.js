c5d6b6a97ee92d73157d3f1b419561083540704e
cynber <26402139+cynber@users.noreply.github.com>
2023-07-29 06:01:30 -0700

    updated search page

--- src/page-search/search.js
+++ src/page-search/search.js
@@ -1,20 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
+
   // Function to fetch and search the 'community' JSON file
   async function searchCommunities(query) {
     try {
       const response = await fetch('https://data.lemmyverse.net/data/community.full.json');
       const data = await response.json();
-  
+
       // Perform the search
       const searchTerm = query.toLowerCase();
       const searchResults = data.filter((community) =>
         community.name.toLowerCase().includes(searchTerm)
       );
-  
+
       // Sort the results by most monthly active users
       searchResults.sort((a, b) => b.counts.users_active_month - a.counts.users_active_month);
-  
+
       return searchResults;
+
     } catch (error) {
       console.error('Error fetching or processing data:', error);
       return [];
@@ -22,7 +24,22 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // Function to display search results on the page
-  function displayResults(results) {
+  async function displayResults(results) {
+
+    // get home instance & type
+    const { selectedInstance } = await browser.storage.local.get('selectedInstance');
+    const { selectedType } = await browser.storage.local.get('selectedType');
+
+    let newURL = '';
+    let hasHomeInstance = false;
+
+    if (!selectedInstance || !selectedType) {
+      // no instance or type selected
+    } else {
+      hasHomeInstance = true;
+      newURL = (selectedType == 'lemmy') ? selectedInstance + '/c/' : selectedInstance + '/m/';
+    }
+
     const resultsContainer = document.getElementById('results');
     resultsContainer.innerHTML = '';
 
@@ -33,10 +50,20 @@ document.addEventListener('DOMContentLoaded', () => {
         const communityCard = document.createElement('div');
         communityCard.classList.add('community-card');
 
+        // If no home instance is selected, use the community's baseurl
+        if (hasHomeInstance == false) {
+          newURL = 'https://' + community.baseurl + '/c/';
+        }
+
+        // Use default icon if none is provided
+        if (!community.icon) {
+          community.icon = '../img/icon-lemm-noIcon.png';
+        }
+
         // Create the HTML content for each community card
         const communityHTML = `
           <img src="${community.icon}" alt="Community Icon" class="community-icon">
-          <h3>${community.name}@${community.baseurl}</h3>
+          <h3><a href="${newURL}${community.name}@${community.baseurl}" target="_blank">${community.name}@${community.baseurl}</a></h3>
           <p><strong>Title:</strong> ${community.title}</p>
           <p><strong>Description:</strong> ${community.desc}</p>
           <p><strong>Subscribers:</strong> ${community.counts.subscribers}</p>
@@ -56,5 +83,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchQuery = document.getElementById('searchQuery').value;
     const searchResults = await searchCommunities(searchQuery);
     displayResults(searchResults);
+  });
+
+  // Open lemmyverse.net
+  document.getElementById('open-lemmyverse').addEventListener('click', () => {
+    window.open('https://lemmyverse.net/', '_blank');
   });
 });
